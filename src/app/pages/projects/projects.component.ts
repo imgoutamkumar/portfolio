92de5a06ebb761eb8d56a7426c7f54b5a557db04
Goutam Kumar <goutamkumar42010no@gmail.com>
2023-05-30 21:22:24 +0530

    third commit

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,18 +9,24 @@ export class ProjectsComponent {
   public projectDetails = [
     {
       project_id: 1,
+      project_img:
+        'https://drive.google.com/file/d/1zsutuxsb9CUqzI-AvhUy-R5KGpj-0JZe/view?usp=share_link',
       thumbnail: 'hfgf',
       title: 'Crud App',
       description: 'sdsfs',
     },
     {
       project_id: 2,
+      project_img:
+        'https://drive.google.com/file/d/1zsutuxsb9CUqzI-AvhUy-R5KGpj-0JZe/view?usp=share_link',
       thumbnail: 'hfgf',
       title: 'Rest API',
       description: 'sdsfs',
     },
     {
-      project_id: 2,
+      project_id: 3,
+      project_img:
+        'https://drive.google.com/file/d/1zsutuxsb9CUqzI-AvhUy-R5KGpj-0JZe/view?usp=share_link',
       thumbnail: 'hfgf',
       title: 'Spotify Clone',
       description: 'sdsfs',
