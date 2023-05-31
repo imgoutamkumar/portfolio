10fc43454cdb35f32411d58bd631a0eb5f5d4fe2
Goutam Kumar <goutamkumar42010no@gmail.com>
2023-05-31 20:18:41 +0530

    sections updated

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,11 +9,11 @@ export class ProjectsComponent {
   public projectDetails = [
     {
       project_id: 1,
-
       thumbnail: 'assets/portfolio.png',
       title: 'Portfolio Website',
       description:
-        'This website is built using Angular FrameWork using HTML, CSS ,TypeScript and Some Popular Libraries like Angular Material, Aos Animation libraries etc.',
+        'This website is built with the help of Angular FrameWork using HTML, CSS ,TypeScript and Some Popular Libraries like Angular Material, Aos Animation libraries etc.',
+      view: 'https://imgoutamkumar.github.io/portfolio/',
     },
     {
       project_id: 2,
@@ -21,13 +21,14 @@ export class ProjectsComponent {
       title: 'Crud App',
       description:
         'This is an Employee Management Web Application, Where you can Create, Read, update, and Delete the employee information.This App Uses Fake JSON Server to handle Server side processes',
+      view: 'https://github.com/imgoutamkumar/crud-app',
     },
     {
       project_id: 3,
-
       thumbnail: 'assets/spotify_clone.png',
       title: 'Spotify Clone',
       description: 'This is a Spotify Front-end UI Clone app.',
+      view: 'https://github.com/imgoutamkumar/spotify-clone',
     },
   ];
 }
